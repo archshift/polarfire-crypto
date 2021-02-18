48bca7482e27223aedef67aca9daa0a99403e6ea
Gui Andrade <gh@archshift.com>
2021-02-18 15:24:31 -0800

    Make driver more robust , fix bugs and link libc statically

--- software/src/crypt/hw.rs
+++ software/src/crypt/hw.rs
@@ -24,46 +24,52 @@ fn wr_reg(reg: AesReg, woffs: usize, dat: u32) {
     assert!(woffs < 0x10000 / 4);
     unsafe {
         let ptr = io_ptr().add(reg as usize).add(woffs);
-        *ptr = dat;
+        ptr.write_volatile(dat);
     }
 }
 fn rd_reg(reg: AesReg, woffs: usize) -> u32 {
     assert!(woffs < 0x10000 / 4);
     unsafe {
         let ptr = io_ptr().add(reg as usize).add(woffs);
-        *ptr
+        ptr.read_volatile()
     }
 }
 
 bf!(AesCtrl[u32] {
     reset : 31:31,
-    ofifo_empty : 2:2,
-    ififo_full : 1:1,
+    ififo_full : 2:2,
+    ofifo_empty : 1:1,
     busy : 0:0
 });
 
-unsafe fn write_key(key: Key) {
+fn write_key(key: Key) {
     for i in 0..8 {
         wr_reg(AesReg::Key, i, key[i]);
     }
 }
-unsafe fn write_ctr(ctr: [u32; 4]) {
+fn write_ctr(ctr: [u32; 4]) {
     for i in 0..4 {
         wr_reg(AesReg::Ctr, i, ctr[i]);
     }
 }
-fn take_word(data: &mut impl Iterator<Item=u8>) -> Option<u32> {
-    let array = [data.next()?, data.next()?, data.next()?, data.next()?];
-    Some(u32::from_le_bytes(array))
+fn take_word(data: &[u8]) -> u32 {
+    let mut array = [0; 4];
+    let len = 4.min(data.len());
+    array[..len].copy_from_slice(&data[..len]);
+    u32::from_le_bytes(array)
 }
-unsafe fn write_block(data: &mut impl Iterator<Item=u8>) -> Option<()> {
-    let mut block = data.take(16);
-    for _ in 0..4 {
-        wr_reg(AesReg::IFifo, 0, take_word(&mut block)?);
+fn write_block(data: &[u8]) -> usize {
+    let len = 16.min(data.len());
+    for i in 0..4 {
+        if let Some(sl) = data.get(i*4 ..) {
+            wr_reg(AesReg::IFifo, 0, take_word(sl));
+        } else {
+            wr_reg(AesReg::IFifo, 0, 0);
+        }
     }
-    Some(())
+    len
 }
-unsafe fn read_block() -> Block {
+fn read_block() -> Block {
     [
         rd_reg(AesReg::OFifo, 0),
         rd_reg(AesReg::OFifo, 0),
@@ -71,7 +77,7 @@ unsafe fn read_block() -> Block {
         rd_reg(AesReg::OFifo, 0)
     ]
 }
-unsafe fn read_write_block(data: &mut impl Iterator<Item=u8>) -> Option<Block> {
+fn read_write_block(data: &mut &[u8]) -> Option<Block> {
     let ctr = AesCtrl::new(rd_reg(AesReg::Ctrl, 0));
     match (ctr.ofifo_empty(), ctr.ififo_full()) {
         (0, 0) => {
@@ -82,11 +88,8 @@ unsafe fn read_write_block(data: &mut impl Iterator<Item=u8>) -> Option<Block> {
             Some(read_block())
         }
         (1, 0) => {
-            if write_block(data).is_none() && ctr.busy() == 0 {
-                None
-            } else {
-                Some(read_block())
-            }
+            write_block(data);
+            None
         }
         (1, 1) => {
             Some(read_block())
@@ -95,21 +98,32 @@ unsafe fn read_write_block(data: &mut impl Iterator<Item=u8>) -> Option<Block> {
     }
 }
 
-pub fn crypt(key: Key, ctr: Ctr, data: &[u8], data_out: &mut [u8]) {
-    unsafe {
-        write_key(key);
-        write_ctr(ctr);
+fn drain_fifo() {
+    while {
+        let ctr = AesCtrl::new(rd_reg(AesReg::Ctrl, 0));
+        ctr.ofifo_empty() == 0 || ctr.busy() == 1
+    } {
+        read_block();
     }
-    let data_blocks = (data.len() + 15) / 16;
-    let data_padding = data_blocks * 16 - data.len();
-    let mut padded_data = data.iter().copied()
-        .chain(iter::repeat(0u8).take(data_padding));
+}
 
-    while let Some(block) = unsafe { read_write_block(&mut padded_data) } {
-        for word in block.iter() {
-            data_out[..4].copy_from_slice(&word.to_le_bytes());
+pub fn crypt(key: Key, ctr: Ctr, mut data: &[u8], mut data_out: &mut [u8]) {
+    write_key(key);
+    write_ctr(ctr);
+
+    drain_fifo();
+    'outer: loop {
+        if let Some(block) = read_write_block(&mut data) {
+            for word in block.iter() {
+                if data_out.len() == 0 { break 'outer }
+
+                let bytes_left = 4.min(data_out.len());
+                data_out[..bytes_left].copy_from_slice(&word.to_le_bytes()[..bytes_left]);
+                data_out = &mut data_out[bytes_left..];
+            }
         }
     }
+    drain_fifo();
 }
 
 fn map() -> MmapMut {
